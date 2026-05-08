710c6a11b5f8bbdccfca7c155ffb865f1c178a33
Jose <damiao.joselucas@gmail.com>
2026-05-09 00:35:14 +1200

    Added a main landing page component, and added it to the main page, and changed the primary color.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Main } from "next/document";
 import Header from "../components/header/Header";
+import MainInfo from "../components/landing-info/main-info";
 
 export default function Home() {
   return <main>
     <Header />
+    <MainInfo />
   </main>;
 }
