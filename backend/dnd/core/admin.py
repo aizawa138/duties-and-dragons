d45b184819ebae3a8a241af511f95291a6ecea52
Robin-code01 <rovan.rangi@gmail.com>
2026-05-08 21:55:40 +1200

    Added the Backend setup

--- backend/dnd/core/admin.py
+++ backend/dnd/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Users, Bosses, CurrentFight, Duties
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Bosses)
+admin.site.register(CurrentFight)
+admin.site.register(Duties)
--- backend/dnd/core/models.py
+++ backend/dnd/core/models.py
@@ -1,3 +1,53 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    # Stats
+    strength = models.FloatField()
+    inteligence = models.FloatField()
+    charisma = models.FloatField()
+    exp = models.FloatField()
+    level = models.IntegerField()
+    user_class = models.CharField(max_length=64)
+    user_hp = models.IntegerField()
+
+
+class Bosses(models.Model):
+    boss_id = models.AutoField(primary_key=True)
+    boss_hp = models.IntegerField()
+    weakness = models.CharField()
+    boss_name = models.CharField()
+
+
+class CurrentFight(models.Model):
+    fight_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(
+        "Users",
+        on_delete=models.CASCADE,
+        related_name="currentFight",
+        blank=False,
+        null=False,
+    )
+    boss_id = models.ForeignKey(
+        "Bosses",
+        on_delete=models.CASCADE,
+        related_name="currentFight",
+        blank=False,
+        null=False,
+    )
+    seconds_left = models.IntegerField()
+
+
+class Duties(models.Model):
+    duty_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(
+        "Users", on_delete=models.CASCADE, related_name="duties", blank=False
+    )
+    description = models.CharField(max_length=7000)
+    strength = models.FloatField(default=0.0)
+    intelligence = models.FloatField(default=0.0)
+    charisma = models.FloatField(default=0.0)
