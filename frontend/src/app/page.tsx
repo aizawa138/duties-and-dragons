c6714c7d12cf8bd9198f64bf9301f406228985ac
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-09 04:21:12 +1200

    feat: initialize app on Home component mount

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
 import Header from "../components/header/Header";
 import MainInfo from "../components/landing-info/main-info";
+import { initializeApp } from "@/lib/initializeApp";
 
 export default function Home() {
+  useEffect(() => {
+    initializeApp();
+  }, []);
+
   return (
     <main className="relative">
       <Header />
