275c52d18fa821423908923ac128f71f09b138bc
Jose <damiao.joselucas@gmail.com>
2026-05-09 10:24:22 +1200

    Added an attack button and added the ability to add tasks.

---
+++ frontend/src/components/attack-button/attack-button.tsx
@@ -0,0 +1,14 @@
+interface AttackButtonProps {
+    onAttack: () => void;
+}
+
+export default function AttackButton({ onAttack }: AttackButtonProps) {
+    return (
+        <button 
+            onClick={onAttack}
+            className="bg-red-600 h-full w-full hover:bg-red-400 text-white font-bold py-2 px-6 border-b-4 border-red-800 hover:border-red-600 rounded active:border-b-0 active:mt-1 transition-all"
+        >
+            ATTACK
+        </button>
+    );
+}
