1f078d77bbc33b77e9b3a7b17c948b2f7bc891ed
Robin-code01 <rovan.rangi@gmail.com>
2026-05-09 02:02:15 +1200

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd backend/dnd && python manage.py runserver 0.0.0.0:$PORT
--- backend/dnd/dnd/settings.py
+++ backend/dnd/dnd/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-5-1vwnp6d%!2r81953*iu&ab8fp6kg7n3#7vz@*2a31g7!#d-*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
