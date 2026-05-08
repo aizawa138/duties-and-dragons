edb04758be0996af53693bd884253d3b3d8a1c5b
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-08 23:49:25 +1200

    feat: replace login button with login and signup modal components in header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,10 +1,17 @@
-import { Button } from "../ui/button/button";
+import LoginModal from "@/src/features/login/LoginModal";
 import Logo from "../logo/Logo";
 
 export default function Header() {
-    return (<header className="flex justify-end items-center p-4 bg-state-900 text-white bg-primary">
-        <div className="flex-1"></div>
-        <div className="flex-1 text-center font-bold"><Logo /></div>
-        <div className="flex-1 text-right"><Button variant="secondary" size="default" className="bg-secondary">Log In</Button></div>
-    </header>);
+  return (
+    <header className="flex justify-end items-center p-4 bg-state-900 text-white bg-primary">
+      <div className="flex-1"></div>
+      <div className="flex-1 text-center font-bold">
+        <Logo />
+      </div>
+      <div className="flex-1 text-right">
+        <LoginModal authenticationType="Login" variant="default" />
+        <LoginModal authenticationType="Signup" variant="secondary" />
+      </div>
+    </header>
+  );
 }
