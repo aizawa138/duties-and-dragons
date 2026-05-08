f405db88848891a48c914349ef625972df8931ff
Jose <damiao.joselucas@gmail.com>
2026-05-09 06:39:52 +1200

    Continued building upon the dashboard page.

---
+++ frontend/src/components/enemy/enemy.tsx
@@ -0,0 +1,9 @@
+import { alikeAngular } from "@/public/fonts";
+
+export default function Enemy() {
+    return (
+        <div className="bg-secondary w-full h-[30vh] flex items-center justify-center rounded-sm shrink-0">
+            I'm an enemy!
+        </div>
+    );
+}  
---
+++ frontend/src/components/player/player.tsx
@@ -0,0 +1,9 @@
+import { alikeAngular } from "@/public/fonts";
+
+export default function Player() {
+    return (
+        <div className="bg-secondary w-full h-[30vh] flex items-center justify-center rounded-sm shrink-0">
+            I'm a player.
+        </div>
+    );
+}  
