9d07ed967584038e20b3b231eaa48cda3d928fd6
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-05-08 21:51:33 +1200

    feat: add Croissant One font configuration

--- frontend/public/fonts.ts
+++ frontend/public/fonts.ts
@@ -1,7 +1,13 @@
-import { Alan_Sans } from "next/font/google";
+import { Alan_Sans, Croissant_One } from "next/font/google";
 
 export const alanSans = Alan_Sans({
   subsets: ["latin"],
   variable: "--font-alan-sans",
+  weight: ["400"],
+});
+
+export const crossaintOne = Croissant_One({
+  subsets: ["latin"],
+  variable: "--font-crossaint-one",
   weight: ["400"],
 });
