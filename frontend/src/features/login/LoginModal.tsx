e9374a7d540181d460f0f415c8e33dd485bc5a15
Robin-code01 <rovan.rangi@gmail.com>
2026-05-09 10:11:11 +1200

    wkrjfnkj

--- frontend/src/features/login/LoginModal.tsx
+++ frontend/src/features/login/LoginModal.tsx
@@ -30,6 +30,7 @@ export default function AuthenticationModal({
         `${process.env.NEXT_PUBLIC_BACKEND_URL!}/api/login/`,
         {
           method: "POST",
+          credentials: "include",
           headers: {
             "Content-Type": "application/json",
             "X-CSRFToken": csrfToken,
@@ -55,6 +56,7 @@ export default function AuthenticationModal({
         `${process.env.NEXT_PUBLIC_BACKEND_URL!}/api/register/`,
         {
           method: "POST",
+          credentials: "include",
           headers: {
             "Content-Type": "application/json",
             "X-CSRFToken": csrfToken,
