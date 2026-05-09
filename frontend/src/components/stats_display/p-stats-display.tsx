0af67b5256a9f1bba75fd034cbb8b578ea8f5150
Jose <damiao.joselucas@gmail.com>
2026-05-09 22:38:38 +1200

    Added stats displays and fixed some formatting.

---
+++ frontend/src/components/stats_display/p-stats-display.tsx
@@ -0,0 +1,19 @@
+interface StatsInfo {
+    str: number;
+    int: number;
+    cha: number;
+    hp: number;
+}
+
+export default function PlayerStats({ str, int, cha, hp }: StatsInfo) {
+    return (
+        <div className="w-full h-[30vh] text-center flex flex-col items-center justify-center rounded-lg p-4 shadow-2xl shadow-slate-950/50 backdrop-blur-xl shrink-0">
+            <ul className="flex flex-col gap-5">
+                <li>HP: {hp}</li>
+                <li>STR: {str}</li>
+                <li>INT: {int}</li>
+                <li>CHA: {cha}</li>
+            </ul>
+        </div>
+    );
+}
