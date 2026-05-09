991947190b5f1822865dd33b8fb1939ea9f9f7a2
Hain Htut <hainhtut45@gmail.com>
2026-05-09 17:48:42 +1200

    update descriptions for character roles in Card component

--- frontend/src/components/ui/card/card.tsx
+++ frontend/src/components/ui/card/card.tsx
@@ -21,14 +21,16 @@ export default function Card({
       className={`group border-2 border-accent bg-primary rounded-2xl p-4 overflow-hidden hover:cursor-pointer transition ${selectedClasses}`}
       onClick={() => onClick(role)}
     >
-      <Image
-        src={src}
-        alt="Image of the classes"
-        height={250}
-        width={250}
-        loading="eager"
-        className={`transition mb-2 ${isSelected ? "scale-110" : "group-hover:scale-110"}`}
-      />
+      <div className="flex justify-center mb-2">
+        <Image
+          src={src}
+          alt="Image of the classes"
+          height={250}
+          width={250}
+          loading="eager"
+          className={`transition mb-2${isSelected ? "scale-110" : "group-hover:scale-110"}`}
+        />
+      </div>
       <h1 className="text-3xl text-background mb-2 font-bold">{role}</h1>
       <p className="text-gray-400">{description}</p>
     </div>
