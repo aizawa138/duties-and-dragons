5edaebfa689d8319fbb550298ef81d9f51f6bc47
Jose <damiao.joselucas@gmail.com>
2026-05-09 19:32:13 +1200

    fixed some formatting issues

--- frontend/src/components/enemy/enemy.tsx
+++ frontend/src/components/enemy/enemy.tsx
@@ -5,8 +5,8 @@ interface EnemyProps {
 export default function Enemy({ s }: EnemyProps) {
     return (
         <div className="w-full h-[30vh] flex flex-col items-center justify-center rounded-lg border border-slate-800 bg-slate-950/90 p-4 shadow-2xl shadow-slate-950/50 backdrop-blur-xl shrink-0">
-            <span className="text-xs uppercase tracking-[0.2em] text-secondary mb-2">Current Target</span>
-            <div className="font-semibold text-lg text-red-400/90">
+            <span className="text-[0.8vw] uppercase tracking-[0.2em] text-secondary mb-2">Current Target</span>
+            <div className="font-semibold text-[1vw] text-red-400/90">
                 I'm {s}!
             </div>
         </div>
