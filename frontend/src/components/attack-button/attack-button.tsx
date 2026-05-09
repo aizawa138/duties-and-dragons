f84c90073a5002bd797be12e92ece7fc4a8052e2
Jose <damiao.joselucas@gmail.com>
2026-05-09 17:36:30 +1200

    changed some things in the frontend

--- frontend/src/components/attack-button/attack-button.tsx
+++ frontend/src/components/attack-button/attack-button.tsx
@@ -6,7 +6,7 @@ export default function AttackButton({ onAttack }: AttackButtonProps) {
     return (
         <button 
             onClick={onAttack}
-            className="bg-red-600 h-full w-full hover:bg-red-400 text-white font-bold py-2 px-6 border-b-4 border-red-800 hover:border-red-600 rounded active:border-b-0 active:mt-1 transition-all"
+            className="bg-red-600 h-full w-full hover:bg-red-400 text-white font-bold py-2 px-6 border-b-4 border-red-800 hover:border-red-600 rounded active:border-b-0 active:mt-1"
         >
             ATTACK
         </button>
